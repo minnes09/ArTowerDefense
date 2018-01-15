c9b8ecb0fa5875d1b9026c11201e719b1f32d6db
Gabriele Minneci <gabriele.minneci@stud-inf.unibz.it>
2018-01-15 17:47:25 +0100

    Spawn buttons linked with coins quantity with delegates

---
+++ Assets/EventTriggeredTextDelegate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventTriggeredTextDelegate : MonoBehaviour {
+
+    public Text text;
+	
+    // Use this for initialization
+	void Start () {
+        CoinsManager.OnCoinChanged += UpdateCoinsValue;
+        int coins = FindObjectOfType<CoinsManager>().Coins;
+        text.text = "Coins: " + coins.ToString();
+    }
+	
+	void UpdateCoinsValue (int coins) {
+        text.text = "Coins: " + coins.ToString();
+	}
+
+    private void OnDestroy()
+    {
+        CoinsManager.OnCoinChanged -= UpdateCoinsValue;
+    }
+}
---
+++ Assets/SpawnableMinionEventTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnableMinionEventTrigger : MonoBehaviour {
+    public int moneyToSpawn;
+    Button button;
+	// Use this for initialization
+	void Start () {
+        button = GetComponent<Button>();
+        CoinsManager.OnCoinChanged += ButtonInteractability;
+    }
+
+    private void ButtonInteractability(int coins)
+    {
+        if (coins >= moneyToSpawn)
+        {
+            button.interactable = true;
+        }
+        else button.interactable = false;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
